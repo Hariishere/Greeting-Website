785475214d9ae30788ec6bb9db826a9f7f162b70
Hariishere <harivignesh3090@gmail.com>
2025-12-26 22:41:57 +0530

    Configure Static file

--- Christmas/settings.py
+++ Christmas/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'Christmas/static/'
 STATIC_ROOT = BASE_DIR / 'Christmas/staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Christmas/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
