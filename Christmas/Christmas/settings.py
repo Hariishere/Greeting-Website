257eda96a3c898778fd039835667d62950fd4748
Hariishere <harivignesh3090@gmail.com>
2025-12-26 22:31:30 +0530

    Configure STATIC_ROOT and whitenoise settings

--- Christmas/Christmas/settings.py
+++ Christmas/Christmas/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Christmas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'christmas/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
